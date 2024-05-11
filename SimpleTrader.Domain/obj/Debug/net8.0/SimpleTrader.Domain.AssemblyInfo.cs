e26027732cfdf21d0bf7550bf79f2763ce9fe9e3
Reza Fathpour <Gholamreza.Fathpour@Gmail.com>
2024-05-11 14:31:25 +0930

    first commit

--- SimpleTrader.Domain/obj/Debug/net8.0/SimpleTrader.Domain.AssemblyInfo.cs
+++ SimpleTrader.Domain/obj/Debug/net8.0/SimpleTrader.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleTrader.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f2af9481e305726ebc275c0066a3c0c273af77")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleTrader.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleTrader.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f2af9481e305726ebc275c0066a3c0c273af77")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
