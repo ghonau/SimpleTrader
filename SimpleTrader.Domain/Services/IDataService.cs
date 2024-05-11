64f2af9481e305726ebc275c0066a3c0c273af77
Reza Fathpour <Gholamreza.Fathpour@Gmail.com>
2024-05-11 14:10:44 +0930

    first commit

---
+++ SimpleTrader.Domain/Services/IDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services
+{
+    internal interface IDataService<T>
+    {
+        
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task<T> Create(T entity); 
+        Task<T> Update(int id ,T entity);
+
+        Task<bool> Delete(int id);
+            
+    }
+}
---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock); 
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-2BV8UEP;Initial Catalog=SimpleTraderDB;User ID=sa;Password=LocalAdmin@123;TrustServerCertificate=true"); 
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
